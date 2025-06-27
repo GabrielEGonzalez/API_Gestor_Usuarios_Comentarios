62eaab95df82063a8503421e527302114bc85030
GabrielEGonzalez <enriquegonzalez123467@gmail.com>
2025-06-26 22:15:56 -0500

    feat: Implementación de rutas de API para comentarios (comment.py)

--- blog_api/app/routes/comment.py
+++ blog_api/app/routes/comment.py
@@ -1,5 +1,6 @@
 from .. import models,schemas,crud,database
 from fastapi import APIRouter , Depends,Path
+from app.crud.comment import obtenerComment,getCommentPOST,createComment,updateComment,deleteComment
 from app.schemas.comment import Comentario
 from typing import Annotated
 from sqlalchemy.orm import Session
@@ -8,21 +9,25 @@ comentario = APIRouter(prefix="/comentarios")
 
 @comentario.get('')
 async def getcomment(db:Session=Depends(database.get_db)):
-    
-    return comentario
+    coment = obtenerComment(db)
+    return coment
 
 @comentario.get('/por_post/{post}')
 async def getPostID(post:int=Path(gt=0),db:Session=Depends(database.get_db)):
-    return
+    getpost = getPostID(db,post)
+    return getpost
 
 @comentario.post('')
 async def createComment(comentario:Comentario,db:Session=Depends(database.get_db)):
-    return
+    
+    comment = createComment(db,comentario)
+    return comment
 
 @comentario.put('/{id}')
 async def updateComment():
     return
 
 @comentario.delete('/{id}')
-async def deleteComment():
-    return
+async def deleteComment(db:Session=Depends(database.get_db),id:int=Path(...,gt=0)):
+    mensaje = deleteComment(db,id)
+    return mensaje
